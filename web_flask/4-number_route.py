035491232364d081c5b914a7c0758a4a117a5621
broline <brolineoronu3@gmail.com>
2023-04-23 13:01:05 -0700

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,56 +1,54 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+"""Starts a Flask web app.
+The applistens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'.
+    /: Displays 'Hello HBNB!'
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
     /python/(<text>): Displays 'Python' followed by the value of <text>.
     /number/<n>: Displays 'n is a number' only if <n> is an integer.
 """
 from flask import Flask
-from flask import abort
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Displays'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>.
+    """Displays'C' followed by <text> value.
     Replaces any underscores in <text> with slashes.
     """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    return "C " + text.replace('_', ' ')
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
+    """Displays'Python' followed by <text> value.
     Replaces any underscores in <text> with slashes.
     """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+    return "Python " + text.replace('_', ' ')
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if n is an integer."""
+    """
+    Displays “n is a number” only if n is an integer
+    """
     return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
+    app.run(host="0.0.0.0", port=5000)
